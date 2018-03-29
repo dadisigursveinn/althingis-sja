4f1b7110accf288b42156d5cd7851f24bf617a55
Dadi Sigursveinn Hardarson <dadi.sigursveinn@gmail.com>
2018-03-29 02:52:08 +0000

    created a script to test out data visualization

---
+++ visualization/test_visualize_script.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(scales)
+library(rvest)
+library(lubridate)
+
+votes <- read_csv("../data/votes.csv", 
+                  col_types = cols(`time` = col_datetime(format = "%Y-%m-%d%.%H:%M:%S")))
+members <- read.csv("../data/members_details.csv") %>% 
+  filter(congress == 148) %>% 
+  select(member_id, name, party_id, congress) %>% 
+  distinct
+parties <- read.csv("../data/parties.csv", na="") %>% 
+  select(party_id, party_name=name, abr=abr_long)
+
+votes_member <- merge(merge(members, votes, by = "member_id"), parties, by="party_id")
+
+
+votes_member %>% 
+  ggplot() +
+  geom_jitter(mapping = aes(x=abr, y=vote), alpha=.5)
