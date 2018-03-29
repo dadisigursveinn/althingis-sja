9d2337a366ccf114f4010a135e57f19a9b04c230
Dadi Sigursveinn Hardarson <dadi.sigursveinn@gmail.com>
2018-03-29 12:08:08 +0000

    created a plot that shows types of votes

--- visualization/test_visualize_script.R
+++ visualization/test_visualize_script.R
@@ -15,7 +15,44 @@ parties <- read_csv("../data/parties.csv", na="") %>%
 
 votes_member <- merge(merge(members, votes, by = "member_id"), parties, by="party_id")
 
+party_votes <- votes_member %>% 
+  select(party_id, party_name, abr, vote_id, vote) %>% 
+  group_by(party_id, abr, vote) %>% 
+  summarise(vote_count=n())
+
+sum_party_votes <- votes_member %>% 
+  select(party_id, party_name, abr, vote_id) %>% 
+  group_by(party_id) %>% 
+  summarise(total_vote_count=n())
+
+party_votes <- merge(party_votes, sum_party_votes, by="party_id") %>%
+  mutate(percentage=(vote_count/total_vote_count))
+
+party_votes %>% 
+  ggplot() +
+  geom_bar(mapping = aes(x=vote, y=percentage, fill=abr),
+           stat="identity",
+           position=position_dodge()) +
+  theme_bw() +
+  labs(
+    title = "Types of votes in session 148 by party",
+    y = "Percent",
+    x = "Vote",
+    fill = "Party"
+  ) +
+  scale_fill_manual(values = c('#EE4D9B',
+                                 '#8EC83E',
+                                 '#199094',
+                                 '#522C7F',
+                                 '#DA2128',
+                                 '#00ADEF',
+                                 '#F6A71D',
+                                 '#488E41'))
 
 votes_member %>% 
   ggplot() +
-  geom_jitter(mapping = aes(x=abr, y=vote), alpha=.5)
+  geom_jitter(mapping = aes(x=abr, y=vote), alpha=.5) +
+  labs(
+    y = "Vote",
+    x = "Party"
+  ) 
