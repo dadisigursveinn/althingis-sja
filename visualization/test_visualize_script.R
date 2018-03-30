2f831f8b2f83606b91ee585092782584bb9de5ec
Dadi Sigursveinn Hardarson <dadi.sigursveinn@gmail.com>
2018-03-30 01:27:45 +0000

    added plot that shows types of votes by hour

--- visualization/test_visualize_script.R
+++ visualization/test_visualize_script.R
@@ -4,8 +4,7 @@ library(rvest)
 library(lubridate)
 library(readr)
 
-votes <- read_csv("../data/votes.csv", 
-                  col_types = cols(`time` = col_datetime(format = "%Y-%m-%d%.%H:%M:%S")))
+votes <- read_csv("../data/votes.csv")
 members <- read_csv("../data/members_details.csv") %>% 
   filter(congress == 148) %>% 
   select(member_id, name, party_id, congress) %>% 
@@ -14,6 +13,38 @@ parties <- read_csv("../data/parties.csv", na="") %>%
   select(party_id, party_name=name, abr=abr_long)
 
 votes_member <- merge(merge(members, votes, by = "member_id"), parties, by="party_id")
+
+votes_member %>% 
+  mutate(vote_time_hour=ifelse((vote_time_hour == 0), 24, vote_time_hour)) %>% 
+  ggplot() +
+  geom_bar(mapping = aes(x=vote_time_hour, fill=vote),
+           position="fill") +
+  scale_y_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1), labels = scales::percent(c(0, 0.25, 0.5, 0.75, 1))) +
+  scale_x_continuous(breaks=seq(7,24,by=1))+
+  theme_bw()+
+  labs(
+    title = "How members vote throughout the day",
+    subtitle="Data for session 148.",
+    y = "",
+    x = "Hour",
+    fill = "Vote"
+  ) +
+  scale_fill_manual(values = c('#999999',
+                               '#666666',
+                               '#F0E442',
+                               '#009E73',
+                               '#D55E00'))
+
+# WIP
+votes_member %>% 
+  group_by(vote, vote_time_hour) %>% 
+  summarise(count=n()) %>% 
+  mutate(perc=count/sum(count)) %>% 
+  ggplot() +
+  scale_y_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1), labels = scales::percent(c(0, 0.25, 0.5, 0.75, 1))) +
+  geom_bar(mapping = aes(x=vote_time_hour, y=perc, fill=vote),
+           stat="identity",
+           position=position_dodge())
 
 party_votes <- votes_member %>% 
   select(party_id, party_name, abr, vote_id, vote) %>% 
